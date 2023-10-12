e1046859ce25d6c4d319d5aaa6d930b643229669
Mike Jackson <perigran_falcon@yahoo.com>
2023-10-12 06:16:20 -0600

    Add files via upload

---
+++ Chapter_01/Hello.java
@@ -0,0 +1,12 @@
+// hello.java
+// Hello World!
+
+class Hello
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+
---
+++ Chapter_01/Test.java
@@ -0,0 +1,11 @@
+// Test.java
+
+class Test
+{
+    public static void main (String[] args)
+    {
+        String text = "This is a test";
+        System.out.println("Message: " + text);
+    }
+}
+
