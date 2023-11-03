ddd71e777a527723ba83901069664a8ef7f16472
Mike Jackson <perigran_falcon@yahoo.com>
2023-11-03 17:04:27 -0500

    Add files via upload

---
+++ Chapter_05/StringLength.java
@@ -0,0 +1,17 @@
+// String_Length.java
+
+class StringLength
+{
+	public static void main (String[] args)
+	{
+		String lang = "Java";
+
+		String series = " in easy steps";
+
+		String title = lang.concat( series );
+
+		System.out.print("\""+title + "\" has ");
+		System.out.println(title.length()+" characters.");
+	}
+}
+
