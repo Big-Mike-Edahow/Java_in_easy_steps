224a9bf5b2b48014ebbfb34d9d050d54b1eaafe3
Mike Jackson <perigran_falcon@yahoo.com>
2023-11-07 14:55:43 -0600

    Add files via upload

---
+++ Chapter_06/Methods.java
@@ -0,0 +1,16 @@
+// Methods.java
+
+class Methods
+{
+	public static void main (String[] args)
+	{
+		System.out.println("\nMessage from the main method.");
+		sub() ;
+	}
+
+	public static void sub ( )
+	{
+		System.out.println("Message from the sub method.\n");
+	}
+}
+
---
+++ Chapter_06/Overload.java
@@ -0,0 +1,27 @@
+// Overload.java
+
+class Overload
+{
+	public static void main (String[] args)
+	{
+		System.out.println("\n" + write(12));
+		System.out.println(write("Twelve"));
+		System.out.println(write(4,16) + "\n");
+	}
+
+	public static String write(int num)
+	{
+        return ("Integer passed is " + num);
+    }
+
+	public static String write(String num)
+    {
+        return ("String passed is " + num);
+    }
+
+	public static String write(int num1, int num2)
+	{
+        return ("Sum Total is " + (num1 * num2));
+    }
+}
+
